a05fa9de13b60480697d6a9c677bd0896af80336
Sebap <sebastien@capsule5.com>
2021-02-13 17:14:10 +0100

    initial

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
-/// <reference types="react-scripts" />
+// / <reference types="react-scripts" />
+declare module 'classnames'
+declare module '*.scss'
+declare module '*.module.scss' {
+  const styles: { [className: string]: string }
+  export default styles
+}
