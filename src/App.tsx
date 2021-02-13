625cb2cbfe4394172590fae0b2d8c73bd36e7dc0
Sebap <sebastien@capsule5.com>
2021-02-13 17:14:22 +0100

    feat form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import cn from 'classnames'
+import FbForm from './form/FbForm'
+import styles from './App.module.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={ cn(styles.App) }>
+      <header className={ cn(styles.header) }>
+        Mappy FizzBuzz
       </header>
+      <section>
+        <FbForm />
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/form/TextField.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import { useField } from 'formik'
+import cn from 'classnames'
+import styles from './TextField.module.scss'
+
+interface Props{
+  name:string
+  type:string
+  value:string
+}
+
+const TextField: React.FC<Props> = ({ name, type, value }) => {
+  const [ field, meta ] = useField({ name, type, value })
+  return (
+    <div className={ cn(styles.group) }>
+      <div className={ cn(styles.line) }>
+        <label>{name}</label>
+        <input { ...field } />
+      </div>
+      {meta.touched && meta.error ? (
+        <div className={ styles.error }>{meta.error}</div>
+      ) : null}
+    </div>
+  )
+}
+
+export default TextField
